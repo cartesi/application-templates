8db4348a4857a5c15042b5a4cb6d037d86f6fabb
Danilo Tuler <tuler@pobox.com>
2023-09-05 00:17:06 -0300

    feat(typescript): using proper typescript typings

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,45 +1,53 @@
-// XXX even though ethers is not used in the code below, it's very likely
-// it will be used by any DApp, so we are already including it here
-import ethers from "ethers";
+import createClient from "openapi-fetch";
+import { components, paths } from "./schema";
 
-const rollup_server = process.env.ROLLUP_HTTP_SERVER_URL;
-console.log("HTTP rollup_server url is " + rollup_server);
+type AdvanceRequestData = components["schemas"]["Advance"];
+type InspectRequestData = components["schemas"]["Inspect"];
+type RequestHandlerResult = components["schemas"]["Finish"]["status"];
+type RollupsRequest = components["schemas"]["RollupRequest"];
+type InspectRequestHandler = (data: InspectRequestData) => Promise<void>;
+type AdvanceRequestHandler = (
+  data: AdvanceRequestData
+) => Promise<RequestHandlerResult>;
 
-async function handle_advance(data) {
+const rollupServer = process.env.ROLLUP_HTTP_SERVER_URL;
+console.log("HTTP rollup_server url is " + rollupServer);
+
+const handleAdvance: AdvanceRequestHandler = async (data) => {
   console.log("Received advance request data " + JSON.stringify(data));
   return "accept";
-}
-
-async function handle_inspect(data) {
-  console.log("Received inspect request data " + JSON.stringify(data));
-  return "accept";
-}
-
-const handlers = {
-  advance_state: handle_advance,
-  inspect_state: handle_inspect,
 };
 
-const finish = { status: "accept" };
+const handleInspect: InspectRequestHandler = async (data) => {
+  console.log("Received inspect request data " + JSON.stringify(data));
+};
 
-(async () => {
+const main = async () => {
+  const { POST } = createClient<paths>({ baseUrl: rollupServer });
+  let status: RequestHandlerResult = "accept";
   while (true) {
-    const finish_req = await fetch(rollup_server + "/finish", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ status: "accept" }),
+    const { response } = await POST("/finish", {
+      body: { status },
+      parseAs: "text",
     });
 
-    console.log("Received finish status " + finish_req.status);
-
-    if (finish_req.status == 202) {
-      console.log("No pending rollup request, trying again");
-    } else {
-      const rollup_req = await finish_req.json();
-      const handler = handlers[rollup_req["request_type"]];
-      finish["status"] = await handler(rollup_req["data"]);
+    if (response.status === 200) {
+      const data = (await response.json()) as RollupsRequest;
+      switch (data.request_type) {
+        case "advance_state":
+          status = await handleAdvance(data.data as AdvanceRequestData);
+          break;
+        case "inspect_state":
+          await handleInspect(data.data as InspectRequestData);
+          break;
+      }
+    } else if (response.status === 202) {
+      console.log(await response.text());
     }
   }
-})();
+};
+
+main().catch((e) => {
+  console.log(e);
+  process.exit(1);
+});
