327930b9ddf3e188c60f6c4fcdaa39381912ed2e
Idogwu Chinonso <idogwuchinonso1@gmail.com>
2025-04-04 11:43:28 +0100

    feat: solidity coprocessor template

---
+++ coprocessor/solidity/test/Counter.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "forge-std-1.9.6/src/Test.sol";
+import {MyContract} from "../src/MyContract.sol";
+
+contract MyContractTest is Test {
+    MyContract public myContract;
+
+    function setUp() public {
+
+    }
+
+
+}
