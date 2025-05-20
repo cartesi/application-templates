0d572341bbb35e33633d5e7f6f4013270d90b960
Danilo Tuler <tuler@pobox.com>
2025-05-20 15:32:58 -0400

    fix typescript template, using 0.10.0 api

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,47 +1,48 @@
 import createClient from "openapi-fetch";
-import { components, paths } from "./schema";
+import type { components, paths } from "./schema";
 
 type AdvanceRequestData = components["schemas"]["Advance"];
 type InspectRequestData = components["schemas"]["Inspect"];
 type RequestHandlerResult = components["schemas"]["Finish"]["status"];
-type RollupsRequest = components["schemas"]["RollupRequest"];
+type RollupRequest = components["schemas"]["RollupRequest"];
 type InspectRequestHandler = (data: InspectRequestData) => Promise<void>;
 type AdvanceRequestHandler = (
   data: AdvanceRequestData
 ) => Promise<RequestHandlerResult>;
 
 const rollupServer = process.env.ROLLUP_HTTP_SERVER_URL;
-console.log("HTTP rollup_server url is " + rollupServer);
+console.log(`HTTP rollup_server url is ${rollupServer}`);
 
 const handleAdvance: AdvanceRequestHandler = async (data) => {
-  console.log("Received advance request data " + JSON.stringify(data));
+  console.log(`Received advance request data ${JSON.stringify(data)}`);
   return "accept";
 };
 
 const handleInspect: InspectRequestHandler = async (data) => {
-  console.log("Received inspect request data " + JSON.stringify(data));
+  console.log(`Received inspect request data ${JSON.stringify(data)}`);
 };
 
 const main = async () => {
   const { POST } = createClient<paths>({ baseUrl: rollupServer });
   let status: RequestHandlerResult = "accept";
   while (true) {
-    const { response } = await POST("/finish", {
+    const { data, response } = await POST("/finish", {
       body: { status },
       parseAs: "text",
     });
 
-    if (response.status === 200) {
-      const data = (await response.json()) as RollupsRequest;
-      switch (data.request_type) {
+    if (response.status === 200 && data) {
+      const request = JSON.parse(data) as RollupRequest;
+      switch (request.request_type) {
         case "advance_state":
-          status = await handleAdvance(data.data as AdvanceRequestData);
+          status = await handleAdvance(request.data as AdvanceRequestData);
           break;
         case "inspect_state":
-          await handleInspect(data.data as InspectRequestData);
+          await handleInspect(request.data as InspectRequestData);
           break;
       }
     } else if (response.status === 202) {
+      // no rollup request available
       console.log(await response.text());
     }
   }
